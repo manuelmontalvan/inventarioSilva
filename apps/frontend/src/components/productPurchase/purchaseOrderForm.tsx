7bb921432a35405e24b454bf282afea1e12b3419
manuelmontalvan <manuel-mont1@hotmail.com>
2025-07-01 21:57:43 -0500

    actualizacion 6

--- apps/frontend/src/components/productPurchase/purchaseOrderForm.tsx
+++ apps/frontend/src/components/productPurchase/purchaseOrderForm.tsx
@@ -172,14 +172,11 @@ export default function PurchaseOrderForm({
         supplierId,
         invoice_number: invoiceNumber,
         notes,
-        items: items.map((item) => {
-          const { name, brand, unit_of_measure, ...rest } = item;
-          return {
-            ...rest,
-            invoice_number: invoiceNumber,
-            supplierId,
-          };
-        }),
+        items: items.map((item) => ({
+          ...item,
+          invoice_number: invoiceNumber,
+          supplierId,
+        })),
       };
 
       await onCreate(newOrder);
