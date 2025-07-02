8d5f957a59923ed2ca587cfc0b85167b0285f388
manuelmontalvan <manuel-mont1@hotmail.com>
2025-07-02 10:38:00 -0500

    deploy backend 7

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -38,7 +38,7 @@ import { PagesModule } from './page/page.module';
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: false, // Solo en desarrollo
+        synchronize: true, // Solo en desarrollo
         migrations: [__dirname + '/migration/*.js'],
         migrationsRun: true,
         cli: {
