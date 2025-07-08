1117e925d39414d09b7a161c057912adfad7cd3c
manuelmontalvan <manuel-mont1@hotmail.com>
2025-07-08 00:40:39 -0500

    ajuste python

--- predictive-service/app/core/config.py
+++ predictive-service/app/core/config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()  # carga desde .env si existe
 
 DB_PARAMS = {
-    "host": os.getenv("DB_HOST", "localhost"),
-    "database": os.getenv("DB_NAME", "inventarioSilva"),
-    "user": os.getenv("DB_USER", "postgres"),
-    "password": os.getenv("DB_PASSWORD", "eros2021"),
-    "port": int(os.getenv("DB_PORT", 5433)),
+    "host": os.getenv("DB_HOST"),
+    "database": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "port": int(os.getenv("DB_PORT")),
 }
