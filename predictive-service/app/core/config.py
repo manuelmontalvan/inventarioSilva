b3898d413375e5d861974af233f98ada04eaaac1
manuelmontalvan <manuel-mont1@hotmail.com>
2025-07-09 15:16:45 -0500

    correcciones en python

--- predictive-service/app/core/config.py
+++ predictive-service/app/core/config.py
@@ -1,9 +1,20 @@
 import os
 from dotenv import load_dotenv
-
+from urllib.parse import urlparse
 load_dotenv()  # carga desde .env si existe
 
-DB_PARAMS = {
+database_url = os.getenv("DATABASE_URL")
+if database_url:
+    result = urlparse(database_url)
+    DB_PARAMS = {
+        "host": result.hostname,
+        "port": result.port,
+        "dbname": result.path[1:],
+        "user": result.username,
+        "password": result.password,
+    }
+else:
+    DB_PARAMS = {
     "host": os.getenv("DB_HOST"),
     "database": os.getenv("DB_NAME"),
     "user": os.getenv("DB_USER"),
--- predictive-service/app/db/connection.py
+++ predictive-service/app/db/connection.py
@@ -3,4 +3,5 @@ from psycopg2.extras import RealDictCursor
 from app.core.config import DB_PARAMS
 
 def get_connection():
+    print("Connecting to DB with params:", DB_PARAMS)
     return psycopg2.connect(**DB_PARAMS, cursor_factory=RealDictCursor)
