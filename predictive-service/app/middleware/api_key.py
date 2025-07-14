ae961456669f1222a93cec9ecdba0bd17d5466e0
manuelmontalvan <manuel-mont1@hotmail.com>
2025-07-14 01:56:59 -0500

    acutalizacion

--- predictive-service/app/middleware/api_key.py
+++ predictive-service/app/middleware/api_key.py
@@ -3,16 +3,20 @@ from starlette.middleware.base import BaseHTTPMiddleware
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # Carga las variables de entorno
-
+load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
 class APIKeyMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        # Evitar protección para documentación
+        # 1️⃣ Deja pasar los preflight CORS (OPTIONS)
+        if request.method == "OPTIONS":
+            return await call_next(request)
+
+        # 2️⃣ Deja pasar la documentación
         if request.url.path.startswith("/docs") or request.url.path.startswith("/openapi.json"):
             return await call_next(request)
 
+        # 3️⃣ Valida la API Key
         api_key = request.headers.get("X-API-Key")
         if api_key != API_KEY:
             raise HTTPException(status_code=401, detail="API key inválida o ausente")
