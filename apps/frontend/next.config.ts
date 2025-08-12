b2934b8d9d73797a4f9fc194a83c2bf1a6a1401e
manuelmontalvan <manuel-mont1@hotmail.com>
2025-08-11 22:26:51 -0500

    images

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ["api.dicebear.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
+      },
+    ],
   },
 };
 
